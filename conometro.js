984b0418fbeeb4f305910e522d6b0dff13d3c16a
Denis <ausdenis@hotmail.com>
2022-12-17 23:03:16 -0300

    mudar

--- conometro.js
+++ conometro.js
@@ -1,12 +1,12 @@
 //Um simples conometro 
-
 let tempo = 0
 let textonumeros = 'Minuto'
+//Valor recebido no conometro,usuario.
 let minutos = 3
-let valorrecebido = 60
+let segundos = 60
 let numero = 0
 setInterval(() => {
-    if(numero==valorrecebido){
+    if(numero==segundos){
        console.log(tempo++,textonumeros)
        if(tempo >=2){
         textonumeros = 'Minutos'
